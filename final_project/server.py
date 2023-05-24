723aad62b70790f93accf0353a76b79ba7366d2f
Killian Levacher <git config --global user.email>
2023-05-24 12:44:51 -0400

    saving

--- final_project/server.py
+++ final_project/server.py
@@ -1,3 +1,4 @@
+import machinetranslation
 from machinetranslation import translator
 from flask import Flask, render_template, request
 import json
@@ -8,17 +9,22 @@ app = Flask("Web Translator")
 def englishToFrench():
     textToTranslate = request.args.get('textToTranslate')
     # Write your code here
-    return "Translated text to French"
+    translation = machinetranslation.translator.english_to_french(textToTranslate)
+    return translation
 
 @app.route("/frenchToEnglish")
 def frenchToEnglish():
     textToTranslate = request.args.get('textToTranslate')
     # Write your code here
-    return "Translated text to English"
+    translation = machinetranslation.translator.french_to_english(textToTranslate)
+    return translation
 
 @app.route("/")
 def renderIndexPage():
     # Write the code to render template
+    from flask import make_response, render_template
+    headers = {'Content-Type': 'text/html'}
+    return make_response(render_template('index.html'),200,headers)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
